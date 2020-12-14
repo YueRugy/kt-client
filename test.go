d20341f83ac6772d641c931f14266553c24547e2
yue <yueweichao0927@gmail.com>
2020-12-14 20:25:50 +0800

    kt-client1

--- test.go
+++ test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"golang.org/x/time/rate"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 func ml(next http.Handler) http.Handler {
@@ -29,10 +32,35 @@ func main() {
 		time.Sleep(time.Second * 1)
 	}*/
 
-	mux := http.NewServeMux()
+	/*mux := http.NewServeMux()
 	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		_, _ = writer.Write([]byte("OK!!!"))
 		//log.Println("2222")
 	})
-	_ = http.ListenAndServe(":8090", ml(mux))
+	_ = http.ListenAndServe(":8090", ml(mux))*/
+
+	rand.Seed(time.Now().UnixNano())
+	for {
+		prod, _ := getProd()
+		fmt.Println(prod)
+		time.Sleep(time.Second * 1)
+	}
+}
+
+type Prod struct {
+	ID    int
+	Title string
+	Price int
+}
+
+func getProd() (Prod, error) {
+	rn := rand.Intn(10)
+	if rn < 6 {
+		time.Sleep(time.Second * 3)
+	}
+	return Prod{
+		ID:    101,
+		Title: "GoLang 教程",
+		Price: 100,
+	}, nil
 }
